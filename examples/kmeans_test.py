e96f634ce421d15ffb188146214aea0992fed860
K Pranit Abhinav <impranitabhinav@gmail.com>
2021-05-14 06:01:51 +0530

    accuracy updated, kmeans test added

--- examples/kmeans_test.py
+++ examples/kmeans_test.py
@@ -1,7 +1,6 @@
 from ravml.cluster import Kmeans
 from sklearn.datasets import load_iris, load_digits
 from sklearn.model_selection import train_test_split
-#from ravcom import inform_server
 
 
 if __name__ == '__main__':
@@ -11,9 +10,7 @@ if __name__ == '__main__':
     iris = load_iris()
 
     X = iris.data[:1000]
-
+    y=iris.target[:1000]
     X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True, test_size=0.3)
-
     k.fit(X_train,3,iter=30)
-    print(k._label,y_train)
     k.plot()
