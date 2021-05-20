34afa6bc290edd1d2af3e340c8880662e60659a3
K Pranit Abhinav <impranitabhinav@gmail.com>
2021-05-20 23:23:16 +0530

    decisionn tree

--- ravml/tree/decision_tree.py
+++ ravml/tree/decision_tree.py
@@ -1,10 +1,9 @@
-import numpy as np
 import ravop.core as R
-
+import sys
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
-
+from ravcom import inform_server
 
 class Node:
     def __init__(self, predicted_class, depth=None):
@@ -26,73 +25,103 @@ class DecisionTreeClassifier:
         self.num_features = None
 
     def fit(self, X, y):
-        self.num_classes = R.unique(R.Tensor(y)).shape_()
-        self.num_features = R.Tensor(X).shape_()
+        X = R.Tensor(X)
+        y = R.Tensor(y)
+        num_classes = y.unique().shape_().gather(R.Scalar(0))
+        num_features = X.shape_().gather(R.Scalar(1))
+        while num_features.status != 'computed':
+            pass
+        self.num_classes = int(num_classes.output)
+        self.num_features = int(num_features.output)
+        print(self.num_classes,self.num_features)
         self.tree = self.grow_tree(X, y)
 
     def find_split(self, X, y):
         ideal_col = None
         ideal_threshold = None
 
-        self.num_classes=np.unique(y).size
-        num_observations = y.size
-
+        num_observations = y.shape_().gather(R.Scalar(0))
+        while num_observations.status!='computed':
+            pass
+        num_observations=int(num_observations.output)
         if num_observations <= 1:
             return ideal_col, ideal_threshold
 
-        y = y.reshape(num_observations, )
-
-        count_in_parent = R.Tensor([np.count_nonzero(y == c) for c in range(self.num_classes)])
-
-
-
-
+        y = y.reshape(shape=[num_observations])
+        count_in_parent = R.Tensor([])
+        for c in range(self.num_classes):
+            count_in_parent = count_in_parent.concat(R.sum(R.equal(y, R.Scalar(c))).expand_dims())
         gini = R.square(count_in_parent.foreach(operation='div', params=num_observations))
-        best_gini = R.sub(R.Scalar(1.0) ,R.sum(gini))
-        while best_gini.status!='computed':
-            pass
-        print(best_gini, "\n", count_in_parent)
-        temp_y = y.reshape(y.shape[0], 1)
+        best_gini = R.sub(R.Scalar(1.0), R.sum(gini))
+        temp_y = y.reshape(shape=[num_observations, 1])
+
 
         for col in range(self.num_features):
-            temp_X = X[:, col].reshape(num_observations, 1)
-            all_data = np.concatenate((temp_X, temp_y), axis=1)
-            sorted_data = all_data[np.argsort(all_data[:, 0])]
-            thresholds, obs_classes = np.array_split(sorted_data, 2, axis=1)
+            temp_X = R.gather(R.transpose(X), R.Scalar(col)).reshape(shape=[num_observations, 1])
+            all_data = R.concat(temp_X, temp_y, axis=1)
 
-            obs_classes = obs_classes.astype(int)
+            column = R.gather(R.transpose(X), R.Scalar(col))
+            ind = column.find_indices(R.sort(R.unique(column)))
+            while ind.status != "computed":
+                pass
+            inform_server()
+            sorted_data = R.Tensor([])
+            for i in ind.output:
+                sorted_data = sorted_data.concat(all_data.gather(R.Tensor(i)))  # need to find another way to sort
+            sorted_data_tpose = sorted_data.transpose()
+            thresholds = sorted_data_tpose.gather(R.Scalar(0)).gather(R.Scalar(0))
+            obs_classes = sorted_data_tpose.gather(R.Scalar(1)).gather(R.Scalar(0))
 
-            num_left = [0] * self.num_classes
-            num_right = count_in_parent.copy()
-
+            num_left = R.Tensor([0] * self.num_classes)  # need ops
+            num_right = count_in_parent
             for i in range(1, num_observations):
-                class_ = obs_classes[i - 1][0]
-                num_left[class_] += 1
-                num_right[class_] -= 1
-                gini_left = 1.0 - sum((num_left[x] / i) ** 2 for x in range(self.num_classes))
-                gini_right = 1.0 - sum((num_right[x] / (num_observations - i)) ** 2 for x in range(self.num_classes))
-                gini = (i * gini_left + (num_observations - i) * gini_right) / num_observations
-                if thresholds[i][0] == thresholds[i - 1][0]:
-                    continue
-                if gini < best_gini:
+                class_ = R.gather(obs_classes, R.Tensor([i - 1]))
+                classencoding = R.one_hot_encoding(class_, depth=self.num_classes).gather(R.Scalar(0))
+                num_left=num_left.add(classencoding)
+                num_right=num_right.sub(classencoding)
+
+                gini_left = R.sub(R.Scalar(1) , R.sum(R.square(R.foreach(num_left, operation='div', params=i))))
+                gini_right = R.sub(R.Scalar(1) , R.sum(R.square(R.foreach(num_right, operation='div', params=num_observations-i))))
+                gini = R.div(R.add(  R.multiply(R.Scalar(i) , gini_left) , R.multiply(R.Scalar(num_observations - i) , gini_right)) , R.Scalar(num_observations))
+
+
+                decision1 = R.logical_and( thresholds.gather(R.Tensor([i])) ,thresholds.gather(R.Tensor([i-1])))
+                decision2 = gini.less(best_gini)
+                while decision2.status != "computed":
+                    pass
+
+
+                print(decision2.output==1)
+                if decision2.output == 1 and decision1 != 1:
                     best_gini = gini
                     ideal_col = col
-                    ideal_threshold = (thresholds[i][0] + thresholds[i - 1][0]) / 2
+                    ideal_threshold = R.div(R.add(thresholds.gather(R.Tensor([i]) ), thresholds.gather(R.Tensor([i - 1])) ), R.Scalar(2))
+        print(ideal_col,ideal_threshold)
         return ideal_col, ideal_threshold
 
     def grow_tree(self, X, y, depth=0):
-        pop_per_class = [np.count_nonzero(y == i) for i in range(self.num_classes)]
+        pop_per_class=R.Tensor([])
+        for c in range(self.num_classes):
+            pop_per_class = pop_per_class.concat(R.sum(R.equal(y, R.Scalar(c))).expand_dims())
         predicted_class = R.argmax(pop_per_class)
         node = Node(predicted_class=predicted_class, depth=depth)
-        node.samples = R.shape(y)
+        node.samples = R.shape(y).gather(R.Scalar(0))
 
         if depth < self.max_depth:
-            col, threshold = self.find_split(X, y)
-            if col and threshold:
-                indices_left = X[:, col] < threshold
-                X_left, y_left = X[indices_left], y[indices_left]
-                indices_right = X[:, col] >= threshold
-                X_right, y_right = X[indices_right], y[indices_right]
+            #col, threshold = self.find_split(X, y)
+            col, threshold=0,R.Tensor([12.895])
+
+            '''
+            
+            '''
+            decision=R.Scalar(col).logical_and(threshold)
+            while decision.status != "computed":
+                pass
+            if decision.output== 1:
+                indices_left = X.transpose().gather(R.Scalar(col)).less(threshold)
+                X_left, y_left = X.gather(indices_left), y.gather(indices_left)
+                indices_right =X.transpose().gather(R.Scalar(col)).greater_equal(threshold)
+                X_right, y_right = X.gather(indices_right), y.gather(indices_right)
                 node.feature_index = col
                 node.threshold = threshold
                 node.left = self.grow_tree(X_left, y_left, depth + 1)
@@ -107,21 +136,20 @@ class DecisionTreeClassifier:
         for obs in X_test:
             node = self.tree
             while node.left:
-                if obs[node.feature_index] < node.threshold:
+                if obs[node.feature_index] < node.threshold.output:
                     node = node.left
                 else:
                     node = node.right
             predictions.append(node.predicted_class)
-        return np.array(predictions)
+        return (predictions)
 
 
 dataset = load_wine()
 X, y = dataset.data, dataset.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
 
-obj = DecisionTreeClassifier(max_depth=7)
-obj.find_split(X_train,y_train)
-#obj.fit(X_train, y_train)
-#pr = obj.predict(X_test)
+obj = DecisionTreeClassifier(max_depth=3)
+obj.fit(X_train[:4], y_train[:4])
+pr = obj.predict(X_test)
 
-#print(f1_score(y_test, pr, average='weighted'))
+# print(f1_score(y_test, pr, average='weighted'))
